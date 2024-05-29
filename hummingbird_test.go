e9e367340f10309b6ed8a214a89dcae145d5f723
Quy Le <lnquy.it@gmail.com>
2024-05-29 22:43:37 +0700

    [main] Add tests

--- hummingbird_test.go
+++ hummingbird_test.go
@@ -2,6 +2,7 @@ package hummingbird
 
 import (
 	"runtime"
+	"sync"
 	"testing"
 )
 
@@ -12,5 +13,40 @@ func BenchmarkMap_Set(b *testing.B) {
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		m.Set(i%noOfItems, "benchmark")
+	}
+}
+
+var (
+	_ok    bool
+	_value string
+)
+
+func BenchmarkMap_Get(b *testing.B) {
+	b.StopTimer()
+	noOfItems := 100_000
+	m := NewMap[int, string](uint64(runtime.GOMAXPROCS(0)), uint64(noOfItems))
+	for i := 0; i < noOfItems; i++ {
+		m.Set(i, "benchmark")
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ok, _value = m.Get(i % noOfItems)
+	}
+}
+
+var _syncMapValue any
+
+func BenchmarkGoSyncMap(b *testing.B) {
+	b.StopTimer()
+	var m sync.Map
+	noOfItems := 100_000
+	for i := 0; i < noOfItems; i++ {
+		m.Store(i, "benchmark")
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		_syncMapValue, _ok = m.Load(i % noOfItems)
 	}
 }
